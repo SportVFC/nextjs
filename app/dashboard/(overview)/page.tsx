411e94121ed09059a421d5f739d2405924002372
SportVFC <virtual.football.contest@gmail.com>
2024-11-19 13:43:46 +0100

    chap 9

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,13 +2,23 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { fetchCardData } from '@/app/lib/data';
+
+// Streaming a component
+import { Suspense } from 'react';
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+  CardsSkeleton,
+} from '@/app/ui/skeletons';
+
+// Grouping components
+import CardWrapper from '@/app/ui/dashboard/cards';
 
 // Page is an async component. This allows you to use await to fetch data.
 // Parallel data fetching : https://nextjs.org/learn/dashboard-app/fetching-data
 export default async function Page() {
-  const revenue = await fetchRevenue(); 
-  const latestInvoices = await fetchLatestInvoices(); // wait for fetchRevenue() to finish
+//  const latestInvoices = await fetchLatestInvoices(); // wait for fetchRevenue() to finish
   const {
     numberOfCustomers,
     numberOfInvoices,
@@ -22,6 +32,7 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        { /*}
         <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
         <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
@@ -29,11 +40,18 @@ export default async function Page() {
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        />
+        />*/}
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
