f83ba3a306fa3cbdc1e4008830ef01080e31ed96
SportVFC <virtual.football.contest@gmail.com>
2024-11-21 15:14:48 +0100

    Chap 12 - Update Invoices

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,37 @@
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+ 
+/*
+In addition to searchParams, page components also accept a prop called params which you can use to access the id. 
+Update your <Page> component to receive the prop:
+*/
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+    const params = await props.params;
+    const id = params.id;
+
+    // Fetch the specific invoice
+    // Import a new function called fetchInvoiceById and pass the id as an argument.
+    // Import fetchCustomers to fetch the customer names for the dropdown.
+    // --> You can use Promise.all to fetch both the invoice and customers in parallel:
+    const [invoice, customers] = await Promise.all([
+        fetchInvoiceById(id),
+        fetchCustomers(),
+    ]);    
+
+    return (
+        <main>
+        <Breadcrumbs
+            breadcrumbs={[
+            { label: 'Invoices', href: '/dashboard/invoices' },
+            {
+                label: 'Edit Invoice',
+                href: `/dashboard/invoices/${id}/edit`,
+                active: true,
+            },
+            ]}
+        />
+        <Form invoice={invoice} customers={customers} />
+        </main>
+    );
+}
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -10,6 +10,9 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 
+// Pass the id to the Server Action
+import { updateInvoice } from '@/app/lib/actions';
+
 export default function EditInvoiceForm({
   invoice,
   customers,
@@ -17,8 +20,12 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  // Instead, you can pass id to the Server Action using JS bind. This will 
+  // ensure that any values passed to the Server Action are encoded.
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   return (
-    <form>
+    //  Pass the id to the Server Action
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
