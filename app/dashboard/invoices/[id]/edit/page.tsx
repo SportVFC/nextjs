db98caa9b91ad146038a08a83b7e215559e3c9db
SportVFC <virtual.football.contest@gmail.com>
2024-11-25 13:28:56 +0100

    Chap 16 - Adding Metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,12 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Invoices Edit',
+};
+
 
 /*
 Now that you know the invoice doesn't exist in your database, let's use notFound to handle it. 
